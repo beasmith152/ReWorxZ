6f67bdadcb13e313d29e6065bd153dc649c458d6
Bea Smith <bsmith152@ewu.edu>
2024-11-21 23:31:21 +0000

    centering content

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,10 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			backgroundImage: {
+        'hero': "url('https://cdn.sanity.io/images/cctd4ker/production/f14b1cd7cd359556864db06cabd95dd8ad8c7bfa-3840x2160.jpg?w=3840&q=75&fit=clip&auto=format')"}
+		},
 	},
 	plugins: [require('@tailwindcss/typography'), require('daisyui')],
 }
