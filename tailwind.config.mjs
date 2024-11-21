78fd3b828d990d28d109082443f79f2975fb2aa5
Bea Smith <bsmith152@ewu.edu>
2024-11-21 23:53:25 +0000

    updated and added fonts

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,6 +3,7 @@ export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		extend: {
+			fontFamily:{chonburi: ['Chonburi', 'sans-serif']},
 			backgroundImage: {
         'hero': "url('https://cdn.sanity.io/images/cctd4ker/production/f14b1cd7cd359556864db06cabd95dd8ad8c7bfa-3840x2160.jpg?w=3840&q=75&fit=clip&auto=format')"}
 		},
