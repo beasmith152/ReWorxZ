40174dbf49c2d8c696a409e8d43ad20de79d2de1
Bea Smith <bsmith152@ewu.edu>
2025-10-03 01:41:08 +0000

    bg change

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -5,7 +5,7 @@ export default {
 		extend: {
 			fontFamily:{chonburi: ['Chonburi', 'sans-serif']},
 			backgroundImage: {
-        'hero': "url('https://i.giphy.com/eNMmoy5ZIa2vyoIjza.webp')"}
+        'hero': "url('https://media3.giphy.com/media/v1.Y2lkPTc5MGI3NjExNDVxejNmcmExbmNmNTVsYmJyMjB0dDFxYm9heDh5ZTUyemQ2M2c5MCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/o27WZxf6IRo1J0G6IP/giphy.gif')"}
 		},
 	},
 	plugins: [require("tw-elements/plugin.cjs"), require('@tailwindcss/typography'), require('daisyui')],
