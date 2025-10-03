16e89efa0f7b9f062fe2e72a45a6c552afa1d603
Bea Smith <bsmith152@ewu.edu>
2025-10-03 02:05:43 +0000

    hero2 36

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -5,7 +5,8 @@ export default {
 		extend: {
 			fontFamily:{chonburi: ['Chonburi', 'sans-serif']},
 			backgroundImage: {
-        'hero': "url('https://media3.giphy.com/media/v1.Y2lkPTc5MGI3NjExNDVxejNmcmExbmNmNTVsYmJyMjB0dDFxYm9heDh5ZTUyemQ2M2c5MCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/o27WZxf6IRo1J0G6IP/giphy.gif')"}
+        'hero': "url('https://media3.giphy.com/media/v1.Y2lkPTc5MGI3NjExNDVxejNmcmExbmNmNTVsYmJyMjB0dDFxYm9heDh5ZTUyemQ2M2c5MCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/o27WZxf6IRo1J0G6IP/giphy.gif')"}, backgroundImage: {
+        'hero2': "url('https://media1.giphy.com/media/v1.Y2lkPTc5MGI3NjExMjQ4OTVvYWUxbnU4bG5pZTE5MDdud2g0YmJ0dThkYWQ4a3E5cHBobyZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/3dVHbijgUdfT4RdevV/giphy.gif')"}
 		},
 	},
 	plugins: [require("tw-elements/plugin.cjs"), require('@tailwindcss/typography'), require('daisyui')],
