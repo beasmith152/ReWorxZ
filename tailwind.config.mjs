abd68a3facad2bd2c0bccb1c728a9cf98a3646ca
Bea Smith <bsmith152@ewu.edu>
2025-10-03 05:39:37 +0000

    bg change

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -7,7 +7,7 @@ export default {
 			rocker: ['Rocker', 'cursive'],},
 			backgroundImage: {
         'hero1': "url('https://media3.giphy.com/media/v1.Y2lkPTc5MGI3NjExNDVxejNmcmExbmNmNTVsYmJyMjB0dDFxYm9heDh5ZTUyemQ2M2c5MCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/o27WZxf6IRo1J0G6IP/giphy.gif')", 
-		'hero2': "url('https://media4.giphy.com/media/v1.Y2lkPTc5MGI3NjExd3drMW0xOTNmbGVnNWU2ZGo3MjFzcjdnc2pnODF4dDR3aXVlamJxciZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/ebkfIyR4vM9ry/giphy.gif')",}
+		'hero2': "url('https://media4.giphy.com/media/v1.Y2lkPTc5MGI3NjExMHgycGF0ZmJneDlqcWQ0N3FwbGNnMGZwYjFyanBzaTY2bmVobTJ3NyZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/RkHyUeaT10CiEuzZHF/giphy.gif')",}
 		},
 	},
 	plugins: [require("tw-elements/plugin.cjs"), require('@tailwindcss/typography'), require('daisyui')],
