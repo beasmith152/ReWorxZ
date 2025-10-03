4e6deecf2a7b661123634259aa4ef0257137c82e
Bea Smith <bsmith152@ewu.edu>
2025-10-03 02:14:15 +0000

    edit

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -6,7 +6,7 @@ export default {
 			fontFamily:{chonburi: ['Chonburi', 'sans-serif']},
 			backgroundImage: {
         'hero1': "url('https://media3.giphy.com/media/v1.Y2lkPTc5MGI3NjExNDVxejNmcmExbmNmNTVsYmJyMjB0dDFxYm9heDh5ZTUyemQ2M2c5MCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/o27WZxf6IRo1J0G6IP/giphy.gif')",
-		'hero2': "url('https://media1.giphy.com/media/v1.Y2lkPTc5MGI3NjExMjQ4OTVvYWUxbnU4bG5pZTE5MDdud2g0YmJ0dThkYWQ4a3E5cHBobyZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/3dVHbijgUdfT4RdevV/giphy.gif')",
+		'hero2': "url('https://media0.giphy.com/media/v1.Y2lkPTc5MGI3NjExczAzY3R4MjgyYTh5OTRlbm9reGpjb3B5dGdxOHdheWpscTE2OHMydSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/izelLjv9Hha4k2qiEg/giphy.gif')",
 		}
 	},
 	plugins: [require("tw-elements/plugin.cjs"), require('@tailwindcss/typography'), require('daisyui')],
