369f0a385e7e63f942ec6e6e209e7765a660bab4
Bea Smith <bsmith152@ewu.edu>
2026-04-21 03:39:30 +0000

    update to other bg for mobile and desktop

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -8,7 +8,7 @@ export default {
 			backgroundImage: {
 				'hero1': "url('https://media0.giphy.com/media/v1.Y2lkPTc5MGI3NjExd3JtdGVjdnRtcWgwcGQzNDZkN3ZvN2t0ZnNxaW91Z3FxazQ3NHRvcCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/ULXAGx1G1LhNJpMKkU/giphy.gif')", 
 			'hero2': "url('/images/bg.png')",
-		'hero3': "url('https://img.freepik.com/premium-vector/abstract-seamless-luxury-dark-blue-gold-geometric-pattern-background_41814-365.jpg')",}
+		'hero3': "url('https://img.freepik.com/free-vector/vintage-flash-tattoos-monochrome-seamless-pattern_225004-1534.jpg?semt=ais_hybrid&w=740&q=80')",}
 	},
 	},
 	plugins: [require("tw-elements/plugin.cjs"), require('@tailwindcss/typography'), require('daisyui')],
